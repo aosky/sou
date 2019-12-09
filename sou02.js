21848d46faeb07ce53b2fe490957d817668b7573
aosky <zdl9994@gmail.com>
2019-12-09 10:15:41 +0800

    Update sou02.js

--- sou02.js
+++ sou02.js
@@ -58,36 +58,190 @@ $(document).ready(function() {
 
 });
 
+const sInfor = document.getElementById("search");
+const sBaidu = document.getElementById("baidu");
+const sGoogle = document.getElementById("google");
+const sBing = document.getElementById("bing");
+const sGitHub = document.getElementById("github");
+const sGitee = document.getElementById("gitee");
+const sZhihu = document.getElementById('zhihu');
+const sJd = document.getElementById('jd');
+const sTaoBao = document.getElementById('taobao');
+const sYouTube = document.getElementById('youtube')
+const sBilibili = document.getElementById("bilibili");
+const sYingshi = document.getElementById("yingshi");
+const sWangpan = document.getElementById("wangpan");
+const sBt = document.getElementById("bt");
+const sFanyi = document.getElementById("fanyi");
+const sIns = document.getElementById("ins");
+const sDaili = document.getElementById("daili");
 
-/*天气插件开始
-天气插件api请地址：www.tianqiapi.com
-*/
-/*天气插件开始*/
-$.ajax({
-    url: 'https://www.tianqiapi.com/api/',
-    data: 'version=v1&city=',
-    dataType: 'JSON',
-    error: function() {
-        console.log('天气插件网络错误！');
-    },
-    success: function(res) {
-        uptime = res.update_time.substring(11);
-        uptime = uptime.substring(0, uptime.length - 3);
-        $('.wea_hover').css('background-image', 'url(/sou/wea/icon/bg/100d.png)');
-        $('.mywth').append(res.city + ' <img class="wea" src="https://www.tianqiapi.com/static/skin/yahoo/' + res.data[0].wea_img + '.png"> ' + res.data[0].wea + ' ' + res.data[0].tem1 + '/' + res.data[0].tem2 + ' ' + res.data[0].air_level);
-        //今日天气
-        $('.wea_top').append('<span class="city"><b>' + res.city + '</b> ' + res.update_time + '更新</span><span class="img" style="background:url(https://www.tianqiapi.com/static/skin/yahoo/' + res.data[0].wea_img + '.png) no-repeat center/contain;"></span> <span class="tem"><b>' + res.data[0].tem + '</b>' + res.data[0].wea + '</span><span class="air">空气质量：' + res.data[0].air_level + '<br>湿度：' + res.data[0].humidity + '%<br>' + res.data[0].win[0] + '：' + res.data[0].win_speed + '</span><span class="air_tips">' + res.data[0].air_tips + '</span>');
-        //今日指数
-        $('.wea_con ul').append('<li>' + res.data[0].index[0].title + '<br><b>' + res.data[0].index[0].level + '</b><br><i>' + res.data[0].index[0].desc + '</i></li>');
-        $('.wea_con ul').append('<li>' + res.data[0].index[3].title + '<br><b>' + res.data[0].index[3].level + '</b><br><i>' + res.data[0].index[3].desc + '</i></li>');
-        $('.wea_con ul').append('<li>' + res.data[0].index[4].title + '<br><b>' + res.data[0].index[4].level + '</b><br><i>' + res.data[0].index[4].desc + '</i></li>');
-        $('.wea_con ul').append('<li>空气污染<br><b>' + res.data[0].index[5].level + '</b><br><i>' + res.data[0].index[5].desc + '</i></li>');
 
-        //未来四天天气
-        $('.wea_foot ul').append('<li>' + res.data[1].day + '<br><img src="https://www.tianqiapi.com/static/skin/yahoo/' + res.data[1].wea_img + '.png"><br><b>' + res.data[1].wea + '</b><br><i>' + res.data[1].tem1 + '°/' + res.data[1].tem2 + '°' + '</i></li>');
-        $('.wea_foot ul').append('<li>' + res.data[2].day + '<br><img src="https://www.tianqiapi.com/static/skin/yahoo/' + res.data[2].wea_img + '.png"><br><b>' + res.data[2].wea + '</b><br><i>' + res.data[2].tem1 + '°/' + res.data[2].tem2 + '°' + '</i></li>');
-        $('.wea_foot ul').append('<li>' + res.data[3].day + '<br><img src="https://www.tianqiapi.com/static/skin/yahoo/' + res.data[3].wea_img + '.png"><br><b>' + res.data[3].wea + '</b><br><i>' + res.data[3].tem1 + '°/' + res.data[3].tem2 + '°' + '</i></li>');
+//isEmpty;
+let isEmpty =function (obj)  {
+    if (obj === null) return true;
+    if (typeof obj === 'undefined') {
+      return true;
     }
+    if (typeof obj === 'string') {
+        let reg = new RegExp("^([ ]+)|([　]+)$");
+        //判断有空格，如果有空格，删除左边空格；
+        if( reg.test(obj)){
+            obj=obj.replace(/(^\s*)/g,"");
+        }
 
-});
-/*天气插件结束*/
+      if (obj === "") {
+        return true;
+      }
+
+
+    }
+    return false;
+}
+//doAction 搜索跳转方法
+/*  
+p：搜索网址前段 
+n: 搜到网址后段
+h: 搜索首页
+*/
+let doAction=function(p,n,s){
+    if(isEmpty(sInfor.value)){
+        window.open(s);
+    }else{
+        window.open(p+sInfor.value+n);
+    }
+    
+}
+
+//拼接网址
+let p='';
+let n='';
+let s=''
+
+//Google
+sGoogle.onclick=function(){
+    p="https://js.mfss.cf/-----https://www.google.com/search?q=";
+   doAction(p,n,s);
+
+}
+
+//BaiDu
+sBaidu.onclick= function()  {
+    p="https://www.baidu.com/s?wd=";
+    doAction(p,'',p);
+};
+document.onkeydown = function (e) { // 回车提交表单
+    // 兼容FF和IE和Opera
+        var theEvent = window.event || e;
+        var code = theEvent.keyCode || theEvent.which || theEvent.charCode;
+        if (code == 13) {   
+          p='https://cn.bing.com/search?q=';
+    n='&ensearch=1&FORM=BESBTB'
+    s='https://www4.bing.com/?scope=web&FORM=BESBTB';
+                doAction(p,n,s);
+            };
+        }
+    
+//Bing
+sBing.onclick=function () {
+    p='https://cn.bing.com/search?q=';
+    n='&ensearch=1&FORM=BESBTB'
+    s='https://www4.bing.com/?scope=web&FORM=BESBTB';
+    doAction(p,n,s);
+}
+
+//GitHub
+sGitHub.onclick= function () {
+    p='https://github.com/search?q=';
+    n='';
+    s='https://github.com'
+    doAction(p,n,s);
+}
+
+//Gitee
+sGitee.onclick = function () {
+    p='https://gitee.com/search?utf8=✓&search=';
+    n='';
+    s='https://gitee.com/'
+    doAction(p,n,s);
+}
+
+//Zhihu
+sZhihu.onclick= function () {
+    p='https://www.zhihu.com/search?q=';
+    n='&type=content&utm_content=search_hot';
+    s='https://www.zhihu.com/'
+    doAction(p,n,s);
+}
+//jd
+sJd.onclick = function () {
+    p='https://search.jd.com/Search?enc=utf-8&keyword=';
+    n='';
+    s='https://search.jd.com/'
+    doAction(p,n,s);
+}
+//TaoBao
+sTaoBao.onclick =function () {
+    p='https://s.taobao.com/search?q=';
+    n='&taoke_type=1';
+    s='https://s.taobao.com/';
+    doAction(p,n,s);
+}
+
+//YouTube
+sYouTube.onclick =function () {
+    p='https://js.mfss.cf/-----https://www.youtube.com/results?search_query=';
+    doAction(p,n,s);
+}
+
+//Instagram
+sIns.onclick =function () {
+    p='https://js.mfss.cf/-----https://www.instagram.com/explore/tags/';
+    doAction(p,n,s);
+}
+
+//Daili
+sDaili.onclick =function () {
+    p='https://inss.tk/-----';
+    doAction(p,n,s);
+}
+
+//Bilibili
+sBilibili.onclick =function () {
+    p='https://search.bilibili.com/all?keyword=';
+    n='';
+    s='https://search.bilibili.com'
+    doAction(p,n,s);
+}
+
+//YingShi
+sYingshi.onclick = function () {
+    p='http://ifkdy.com/?q=';
+    n='&p=1';
+    s='http://ifkdy.com/';
+    doAction(p,n,s);
+}
+
+//Wangpan
+sWangpan.onclick = function () {
+    p='http://www.panuso.com/s/';
+    n='.html';
+    s='http://www.panuso.com'
+    doAction(p,n,s);
+}
+
+//BT
+sBt.onclick = function () {
+    p='https://www.bturl.cc/search/';
+    n='_ctime_1.html';
+    s='https://www.bturl.cc'
+    doAction(p,n,s);
+}
+
+//翻译
+sFanyi.onclick = function (){
+    p='https://fanyi.baidu.com/#auto/zh/';
+    n='';
+    s='https://fanyi.baidu.com/';
+    doAction(p,n,s);
+}
